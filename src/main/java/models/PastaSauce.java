3621f7cd71fce2db3b0057a8d213ea0555a92a98
Joao <johnynecas@hotmail.com>
2017-12-03 17:27:46 +0000

    created cosntructor for PastaSauce

--- src/main/java/models/PastaSauce.java
+++ src/main/java/models/PastaSauce.java
@@ -1,4 +1,14 @@
 package models;
 
-public class PastaSauce {
+public class PastaSauce extends Item {
+
+    private String flavour;
+
+
+    public PastaSauce(String name,double price, String flavour){
+        super(name, price);
+        this.flavour= flavour;
+
+    }
+    
 }
