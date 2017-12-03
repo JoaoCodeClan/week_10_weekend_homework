067a33fd190279c77c0e6fb11aed990d0edf0c3e
Joao <johnynecas@hotmail.com>
2017-12-03 17:00:10 +0000

    created constructor for eggs

--- src/main/java/models/Eggs.java
+++ src/main/java/models/Eggs.java
@@ -1,4 +1,12 @@
 package models;
 
-public class Eggs {
+public class Eggs extends Item {
+    private String type;
+
+    public Eggs(String name, double price, String type){
+        super(name, price);
+        this.type = type;
+    }
+
+    
 }
