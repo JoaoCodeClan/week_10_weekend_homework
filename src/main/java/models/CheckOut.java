1f5c1f97ae91e44b264082d87366031a3aeb0403
Joao <johnynecas@hotmail.com>
2017-12-03 19:48:15 +0000

    created and tested method in checout to calculate total depending on condtions and set clients bill to that amount

--- src/main/java/models/CheckOut.java
+++ src/main/java/models/CheckOut.java
@@ -8,4 +8,24 @@ public class CheckOut {
         this.client = client;
     }
 
+    public void proceedCheckOut() {
+        double totalExpense = 0;
+        if (this.client.getBasketTotal() >= 20 && this.client.checkForLoyaltyCard() == true) {
+            double discount = client.getBasketTotal() - (client.getBasketTotal() * 0.1);
+            double discount1 = discount - (discount * 0.02);
+            totalExpense += discount1;
+        }
+        else if (this.client.getBasketTotal() >= 20 && this.client.checkForLoyaltyCard() == false) {
+            double discount = client.getBasketTotal() - (client.getBasketTotal() * 0.1);
+            totalExpense += discount;
+        }
+        else if (this.client.getBasketTotal() < 20 && this.client.checkForLoyaltyCard() == true) {
+            double discount = client.getBasketTotal() - (client.getBasketTotal() * 0.02);
+            totalExpense += discount;
+        }
+        else {
+            totalExpense += client.getBasketTotal();
+        }
+        this.client.setBill(totalExpense);
+    }
 }
--- src/test/java/CheckOutTest.java
+++ src/test/java/CheckOutTest.java
@@ -2,11 +2,13 @@ import models.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CheckOutTest {
+import static org.junit.Assert.assertEquals;
 
+public class CheckOutTest {
+    CheckOut checkout;
+    CheckOut checkout1;
     Client client;
     Client client1;
-    Client client2;
     Eggs item1;
     Bread item2;
     PastaSauce item3;
@@ -18,12 +20,56 @@ public class CheckOutTest {
 
         client= new Client(LoyaltyCard.YES);
         client1= new Client(LoyaltyCard.NO);
-        client2= new Client(LoyaltyCard.YES);
+        checkout= new CheckOut(client);
+        checkout1= new CheckOut(client1);
         item1= new Eggs("Margaret's", 20.0, "Free Range");
         item2= new Bread("Bloomer", 3.5, "medium");
         item3= new PastaSauce("Toni's", 5.0, "BitterSweet");
     }
 
+
+
+    @Test
+
+    public void canGetTotalAndSetBillForClientWithExpensesOver20AndLoyaltyCard(){
+        client.buyItem(item1);
+        client.buyItem(item3);
+        checkout.proceedCheckOut();
+        double actual = client.getBill();
+        assertEquals(22.05, actual, 0.01);
+
+    }
+
+    @Test
+
+    public void canGetTotalAndSetBillForClientWithExpensesOver20AndNoLoyaltyCard(){
+        client1.buyItem(item1);
+        client1.buyItem(item3);
+        checkout1.proceedCheckOut();
+        double actual = client1.getBill();
+        assertEquals(22.5, actual, 0.01);
+
+    }
+
+    @Test
+
+    public void canGetTotalAndSetBillForClientWithExpensesBellow20AndWithLoyaltyCard(){
+        client.buyItem(item3);
+        checkout.proceedCheckOut();
+        double actual = client.getBill();
+        assertEquals(4.9, actual, 0.01);
+
+    }
+
+    @Test
+
+    public void canGetTotalAndSetBillForClientWithExpensesBellow20AndWithoutLoyaltyCard(){
+        client1.buyItem(item2);
+        checkout1.proceedCheckOut();
+        double actual = client1.getBill();
+        assertEquals(3.5, actual, 0.01);
+
+    }
 
 
 
